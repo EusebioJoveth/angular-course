963e5f2c537ab48645a4fc79c8eb172f869cd1ea
Eusebio Joveth <eusebio.joveth@porlande.com>
2024-07-03 12:40:21 +0100

    trabalhando com signal em entradas

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,14 +10,8 @@ import { Component, computed, input } from '@angular/core';
 })
 export class UserComponent {
 
- // @Input({required: true}) avatar!: string;
- // @Input({required: true}) name!: string;
  avatar = input.required<string>();
  name = input.required<string>();
-
- /* get imagePath() {
-    return 'assets/users/' + this.avatar
-  }*/
 
     imagePath = computed(() =>{
       return 'assets/users/' + this.avatar();
