bb3cbd0ad3d45c2e1647c75b9ae5dc56ec9566c4
Eusebio Joveth <eusebio.joveth@porlande.com>
2024-07-03 10:48:28 +0100

    feat: trabalhando com signal

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -15,10 +15,6 @@ export class UserComponent {
   selectedUser = signal(DUMMY_USERS[randomIndex]) ;
   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
-  /*get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar
-  }*/
-
   onSelectUser(){
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser.set (DUMMY_USERS[randomIndex]);
