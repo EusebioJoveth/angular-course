a6c2be7f1043f73bbb98aff684876da8dda2b35d
Eusebio Joveth <eusebio.joveth@porlande.com>
2024-07-03 10:44:12 +0100

    feat: trabalhando com signal

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Tatiana Adelina',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Jaqueline Bastos',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Eusébio Joveth',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Henriques Vidal',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Kambulu Purcel',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Victorino Buta',
+    avatar: 'user-6.jpg',
+  },
+];
