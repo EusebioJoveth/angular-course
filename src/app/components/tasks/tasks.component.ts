4b062bef9cb5c40839a5d42a4b1af7550649e126
Eusebio Joveth <eusebio.joveth@porlande.com>
2024-07-05 17:00:12 +0100

    feat: add task implemented

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTask } from '../../models/new-task';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -12,6 +14,7 @@ export class TasksComponent {
 
   @Input({required: true}) userId!:string;
   @Input({required: true}) userName ?: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -43,5 +46,24 @@ export class TasksComponent {
 
   onCompleteTask(id: string){
     this.tasks = this.tasks.filter((task)=> task.id !==id)
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData:NewTask){
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    });
+    this.isAddingTask = false;
   }
 }
